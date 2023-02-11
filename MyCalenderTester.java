ec6f00de9142b5a29148df8240bca942bacf90e6
Nitisuk Tatiyasuntorn <t.nitsuk@gmail.com>
2023-02-10 22:27:58 -0800

    startup calender finished

--- MyCalenderTester.java
+++ MyCalenderTester.java
@@ -36,30 +36,47 @@ public class MyCalenderTester {
 	        public static void printCalendar(LocalDate c)
 	        {  
 	            //System.out.print(c.getDayOfWeek());
-	            System.out.print(" ");
 	            //System.out.print(c.getDayOfMonth());
-	            System.out.print(c.getMonth());
+	            System.out.print("\t" + c.getMonth());
 	            System.out.print(" ");
 	            System.out.println(c.getYear());
-	            String header = "   Sun   Mon   Tue   Wed   Thu   Fri   Sat";
+	            String header = "Sun\tMon\tTue\tWed\tThu\tFri\tSat";
 	            System.out.println(header);
-	            int j = c.getDayOfMonth();
-	            System.out.println(j);
+	           	
+				LocalDate x = LocalDate.of(c.getYear(), c.getMonth(), 1);
+				int i = 0;
+			   	int firstDayOfMonth = x.getDayOfWeek().getValue();
+	           	while( i < firstDayOfMonth ) {
+					System.out.print("\t");
+					i++;
+			  	}
+				//System.out.println();
+
+				//int dayConverter = x.getDayOfWeek().getValue();
+
+				for( int dayCounter = 1; dayCounter <= x.lengthOfMonth(); dayCounter++ ) {
+					if((dayCounter + 2) % 7  == 0) {
+						System.out.println();
+					}
+					if( dayCounter == c.getDayOfMonth()) {
+						System.out.print("[" + dayCounter + "]\t");
+					}
+					else {
+						System.out.print(dayCounter + "\t");
+					}
+					
+				}
 	            
-	            j = 16%7;
-	            System.out.println(j);
-	            
+
 	            // To print a calendar in a specified format. 
-	            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d");
-	            System.out.println("    " + formatter.format(c.minusDays(c.getDayOfMonth()-1)));
-	            int i = 1;
-	            while(i <= c.getMonth().minLength()) {
-	            	System.out.println(" " + i + " ");
-	            	i++;
-	            }
+	            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d ");
+	            
+	           
 	            
 	            // To figure out the day of week of the 1st day of the given month
-	            LocalDate x = LocalDate.of(c.getYear(), c.getMonth(), 1); 
+	            //LocalDate x = LocalDate.of(c.getYear(), c.getMonth(), 1);
+				System.out.println("\n" + formatter.format(c));
+ 
 	            System.out.println(x.lengthOfMonth() + " is the day of " + c.getMonth() + " 1."); // enum value as it is
 	            System.out.println(x.getDayOfWeek().getValue() + " is an integer value corresponding "
 	            		+ " to " + x.getDayOfWeek()); // int value corresponding to the enum value

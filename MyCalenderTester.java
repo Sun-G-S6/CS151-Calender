143f2df3432363a16319cb1d36a9a144ec025884
Nitisuk Tatiyasuntorn <t.nitsuk@gmail.com>
2023-03-02 01:01:56 -0800

    almost can add recurring events

--- MyCalenderTester.java
+++ MyCalenderTester.java
@@ -19,12 +19,14 @@ public class MyCalenderTester {
 		MyCalender calender = new MyCalender();
 		Event calenderEvent = new Event();
 		LocalDate eventDay = LocalDate.now();
+		LocalDate eventEndDay = LocalDate.now();
 		LocalTime eventTime = LocalTime.now();
+		LocalTime eventEndTime = LocalTime.now();
 		LocalDateTime eventDateTime = LocalDateTime.now();
 		String eventName, eventDetails;
 
 		int month, startDay, endDay, year, begHour, endHour, begMins, endMins;
-		int[] daysInWeek = {};
+		int[] daysInWeek = {0,0,0,0,0,0,0};
 		//DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm H:mm M/dd/yy M/dd/yy");
 
 		try {
@@ -102,10 +104,7 @@ public class MyCalenderTester {
 					String days = eventDetailsArray[0];
 					String[] recurringDays = days.split("");
 					
-					/*
-					 * probably the repeating days to string and compare future ones? 
-					 * idfk
-					 */
+					
 					for(int i = 0; i < recurringDays.length; i++) {
 						switch(recurringDays[i]) {
 							case "S":
@@ -130,19 +129,7 @@ public class MyCalenderTester {
 								daysInWeek[i] = 6;
 								break;
 						}
-					// code logic here????
 					
-					for(int j = 0; j < recurringDays.length; j++) {
-						
-					}
-
-
-
-
-
-
-
-					}
 					// beginning date
 					Scanner detailScanner = new Scanner(eventDetailsArray[3]);
 					detailScanner.useDelimiter("[^0-9]+");
@@ -150,6 +137,7 @@ public class MyCalenderTester {
 					startDay = detailScanner.nextInt();
 					year = 2000 + detailScanner.nextInt();
 					detailScanner.close();
+					eventDay = LocalDate.of(year, month, startDay);
 
 					// ending date
 					detailScanner = new Scanner(eventDetailsArray[4]);
@@ -158,6 +146,8 @@ public class MyCalenderTester {
 					endDay = detailScanner.nextInt();
 					year = 2000 + detailScanner.nextInt();
 					detailScanner.close();
+					eventEndDay = LocalDate.of(year, month, endDay);
+					int endDate = eventEndDay.getDayOfYear();
 
 					// beginning time
 					detailScanner = new Scanner(eventDetailsArray[1]);
@@ -173,27 +163,43 @@ public class MyCalenderTester {
 					endHour = detailScanner.nextInt();
 					endMins = detailScanner.nextInt();
 					detailScanner.close();
+					eventEndTime = LocalTime.of(endHour, endMins);
 
-					// Do something with the event data
+					
+					int numDays = daysInWeek.length;
+					int dayDiff = 0;
+					for(int j = 0; j < daysInWeek.length; j++) {
+						if (j==0) {
+							startDay = eventDay.getDayOfYear();
+							for(int z = startDay; z <= endDate;) {
+								eventDay = eventDay.ofYearDay(year, z);
+								eventDateTime = LocalDateTime.of(eventDay, eventTime);
+								calenderEvent.addBegDateTime(eventDateTime);
+								eventDateTime = LocalDateTime.of(eventDay, eventEndTime);
+								calenderEvent.addEndDateTime(eventDateTime);
+								z += 7;
+							}
+						} 
+						else {
+							dayDiff = daysInWeek[j+1] - daysInWeek[j];
+							startDay = eventDay.getDayOfYear() + dayDiff;
+							for(int z = startDay; z <= endDate;) {
+								eventDay = eventDay.ofYearDay(year, z);
+								eventDateTime = LocalDateTime.of(eventDay, eventTime);
+								calenderEvent.addBegDateTime(eventDateTime);
+								eventDateTime = LocalDateTime.of(eventDay, eventEndTime);
+								calenderEvent.addEndDateTime(eventDateTime);
+								z += 7;
+							}
+						}
+					}
 				}
-
-				/*
-				 * Example singular event
-				 * St Patrick's Day
-				 * 3/17/23 19:00 21:30
-				 * 
-				 * S-SUNDAY M-MONDAY T-TUESDAY W-WEDNESDAY R-THURSDAY F-FRIDAY A-SATURDAY
-				 * 
-				 * Example repeating event
-				 * CS151 Lecture
-				 * TR 9:00 10:15 1/24/23 5/23/23
-				 * 0 1 2 3 4
-				 */
-
-				
+			}
+			
 			}
 			eventScanner.close();
-		} catch (IOException e) {
+		}
+		 catch (IOException e) {
 			System.out.println("Error: File not found");
 			e.printStackTrace();
 		}
